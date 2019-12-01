6d4c22a149d796e7d5a3f4532c3cec02c50e5dac
Brandon Burrus <brandon@burrus.io>
2019-11-30 18:07:49 -0700

    style(linting): Fixed linting issues

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+/* eslint-disable jest/expect-expect */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("app.js", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
 });
